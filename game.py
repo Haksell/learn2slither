0b5d7b3dacb1473202db3b9a85f292ef0f59b6a0
Axel Brisse <axel.brisse@gmail.com>
2024-12-14 02:54:51 +0100

    .

--- game.py
+++ game.py
@@ -10,56 +10,15 @@ _SNAKE_COLOR = (96, 96, 255)
 _BACKGROUND_COLOR = (50, 50, 50)
 
 
-class Puzzle:
-    def __init__(self, tiles, height, width, *, goal=None):
-        self.__height = height
-        self.__width = width
-        self.__tiles = tiles
-
-    def __len__(self):
-        return len(self.__tiles)
-
-    def __iter__(self):
-        yield from self.__tiles
-
-    def __getitem__(self, idx):
-        return self.__tiles[idx]
-
-    def __str__(self):
-        return "\n".join(
-            " ".join(
-                f"{self[y*self.__width+x]:{self.padding}}" for x in range(self.__width)
-            )
-            for y in range(self.__height)
-        )
-
-    @property
-    def height(self):
-        return self.__height
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def goal(self):
-        return self.__goal
-
-    def is_correct(self, i):
-        return self[i] == self.__goal[i]
-
-    def update_goal(self, goal):
-        self.__goal = goal
-
-
 class Game(pyglet.window.Window):
-    def __init__(self, puzzle):
-        self.__puzzle = puzzle
+    def __init__(self, *, width=10, height=10):
+        self.__width = width
+        self.__height = height
         self.__tile_size = self.__compute_tile_size()
         self.__padding = round(_TILE_PADDING * self.__tile_size)
         super().__init__(
-            width=self.__tile_size * puzzle.width + 2 * self.__padding,
-            height=self.__tile_size * puzzle.height + 2 * self.__padding,
+            width=self.__tile_size * width + 2 * self.__padding,
+            height=self.__tile_size * height + 2 * self.__padding,
             caption="learn2slither",
         )
         self.__batch = self.__make_batch()
@@ -68,29 +27,27 @@ class Game(pyglet.window.Window):
 
     def __compute_tile_size(self):
         screen = pyglet.canvas.get_display().get_default_screen()
-        h = screen.height / self.__puzzle.height
-        w = screen.width / self.__puzzle.width
+        h = screen.height / self.__height
+        w = screen.width / self.__width
         return min(_MAX_TILE_SIZE, int(min(h, w) * _MAX_SCREEN_PROPORTION))
 
     def __make_batch(self):
         batch = pyglet.graphics.Batch()
         visible_tile_size = (1 - 2 * _TILE_PADDING) * self.__tile_size
-        for i in range(len(self.__puzzle)):
-            y, x = divmod(i, self.__puzzle.width)
-            color = [
-                _SNAKE_COLOR,
-                _GOOD_APPLE_COLOR,
-                _BAD_APPLE_COLOR,
-                _BACKGROUND_COLOR,
-            ][i % 4]
-            pyglet.shapes.Rectangle(
-                x * self.__tile_size + 2 * self.__padding,
-                self.height - (y * self.__tile_size + self.__tile_size),
-                visible_tile_size,
-                visible_tile_size,
-                color=color,
-                batch=batch,
-            )
+        for y in range(self.__height):
+            for x in range(self.__width):
+                color = [
+                    [_SNAKE_COLOR, _BACKGROUND_COLOR][x & 1],
+                    [_GOOD_APPLE_COLOR, _BAD_APPLE_COLOR][x & 1],
+                ][y & 1]
+                pyglet.shapes.Rectangle(
+                    x * self.__tile_size + 2 * self.__padding,
+                    self.height - (y * self.__tile_size + self.__tile_size),
+                    visible_tile_size,
+                    visible_tile_size,
+                    color=color,
+                    batch=batch,
+                )
         return batch
 
     def on_draw(self):
@@ -111,8 +68,8 @@ class Game(pyglet.window.Window):
 
 
 def __main():
-    puzzle = Puzzle(list(range(100)), 10, 10)
-    Game(puzzle).run()
+    for i in range(1, 31):
+        Game(width=i, height=i).run()
 
 
 if __name__ == "__main__":
